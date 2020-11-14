629de4486f9766ad04de31522e0bf4426dc6b3fb
prachiwhitehat <71639231+prachiwhitehat@users.noreply.github.com>
2020-11-14 08:11:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine,world;
+
+var box1, box2;
+
+function setup() {
+
+  createCanvas(400,400);
+  
+  engine = Engine.create();
+
+  world = engine.world;
+
+ box1 = new Box(200,50,30,40);
+
+ box2 = new Box(100,50,30,30);
+
+
+ console.log(box);
+  
+
+ 
+}
+
+function draw() {
+
+background(0);
+
+Engine.update(engine);
+
+box1.display();
+box2.display();
+  
+}
